3a19d49a325a0d949ea9b0a7e562e669e353778d
Anastasia Fongrad <fongradnastya@gmail.com>
2023-04-18 16:29:01 +0700

    Added opasity animation

--- js/animation.js
+++ js/animation.js
@@ -3,22 +3,51 @@ const animItems = $("._anim_items");
 if ($("._anim_items")){
     $(window).scroll(function()
     {
-        if ($(this).scrollTop() > 0) {
-            $("._anim_items").each(function(){
-                new_animation($(this));
+        if ($(this).scrollTop() > 300) {
+            $("#s1").find("._anim_items").each(function(){
+                hide_animation($(this));
             })
         }
+        if ($(this).scrollTop() > 0) {
+            $("#s1").find("._anim_items").each(function(){
+                show_animation($(this));
+            })
+        }
+        if($(this).scrollTop() > 900){
+            if($(this).scrollTop() < 1450){
+                $(".categories").find("._anim_items").each(function(){
+                    show_animation($(this));
+                })
+            }
+        }
+        if($(this).scrollTop() > 1400){
+            $(".categories").find("._anim_items").each(function(){
+                hide_animation($(this));
+            })
+        }
+        
     });
 }
 
-function new_animation(block){
+function show_animation(block){
+    
     block.animate(
         {
-            width: "300px",
-            height: "300px"
+            opacity: 1, 
+            right: 0
         }, 
-        1000, "linear"
+        'slow'
     );
 }
 
-new_animation();
+
+function hide_animation(block){
+    console.log(2);
+    block.animate(
+        {
+            opacity: 0, 
+            right: 0
+        }, 
+        'slow'
+    );
+}
